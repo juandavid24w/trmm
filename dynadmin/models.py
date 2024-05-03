119145dbd61488b08fdfa1a00a3f967b3c9ca66f
Juan0402 <davidbare0402@gmail.com>
2024-05-03 16:01:08 -0300

    dynadmin: models: Add method converst to dict

--- dynadmin/models.py
+++ dynadmin/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms.models import model_to_dict
 from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
 from solo.models import SingletonModel
@@ -19,6 +20,9 @@ class SiteConfiguration(SingletonModel):
 
     def __str__(self):
         return gettext("Configuração do site")
+
+    def as_dict(self):
+        return model_to_dict(self)
 
     class Meta:
         verbose_name = _("Configuração do site")
