20ef484fee3ffacd62d66bb940dca8c389f17b3b
Juan0402 <davidbare0402@gmail.com>
2024-05-04 20:07:32 -0300

    books: import_books: Read code from origin

--- books/management/commands/import_books.py
+++ books/management/commands/import_books.py
@@ -7,12 +7,17 @@ from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from ...models import Book, Classification, Location
+from ...models import Book, Classification, Location, Specimen
 from ...validators import validate_isbn
 
 User = get_user_model()
 csv_file = settings.BASE_DIR / "all_books.csv"
 
+
+def get_publisher(raw):
+    if pd.isnull(raw):
+        return None
+    return re.sub(r"[-,\d\s\.]*$", "", raw).title()
 
 def get_author_names(raw):
     names = raw.split(";")[0]
@@ -20,7 +25,7 @@ def get_author_names(raw):
     if "," in names:
         names = " ".join(reversed(names.split(",")))
 
-    names = re.sub("[,.\d]", "", names)
+    names = re.sub(r"[,.\d]", "", names)
     names = [name.strip() for name in names.split()]
     return [name + "." if len(name) == 1 else name for name in names]
 
@@ -39,6 +44,11 @@ class Command(BaseCommand):
             except ValidationError:
                 continue
             title = row["titulo"].strip("/ ").strip()
+
+            if Book.objects.filter(isbn=row["isbn"]).exists():
+                self.stdout.write(f"{title} already exists.")
+                continue
+
             self.stdout.write(f"Importing {title}...")
 
             location, _ = Location.objects.get_or_create(
@@ -56,15 +66,21 @@ class Command(BaseCommand):
                 title=title,
                 author_first_names=" ".join(author_names[:-1]),
                 author_last_name=author_names[-1],
-                publisher=row["editora"],
+                publisher=get_publisher(row["editora"]),
                 classification=classification,
                 creation_date=timezone.now(),
                 last_modified=timezone.now(),
             )
 
             book.save()
+            book_code = row["etiqueta"]
 
-            for _ in range(
+            for i in range(
                 1 if pd.isnull(row["exemplares"]) else int(row["exemplares"])
             ):
-                book.specimens.create()
+                code = f"{book_code} Ex {i + 1}"
+                sp = Specimen(book=book)
+
+                if code:
+                    sp.code = code
+                sp.save()
