6445248dd5c892ba320e6c70bec07a0619c6f33f
Juan0402 <davidbare0402@gmail.com>
2024-05-06 15:57:21 -0300

    loans: Use default filter in admin change list

--- loans/filters.py
+++ loans/filters.py
@@ -7,12 +7,15 @@ from django.utils.translation import gettext_lazy as _
 class LoanStatusFilter(admin.SimpleListFilter):
     title = _("status do empréstimo")
     parameter_name = "loan_status"
+    default_lookup = "not_returned"
 
     def lookups(self, *args):
         return [
-            ("returned", _("Devolvidos")),
+            ("not_returned", _("Não devolvidos")),
             ("late", _("Atrasados")),
-            ("running", _("Emprestados")),
+            ("running", _("No prazo")),
+            ("all", _("Todos")),
+            ("returned", _("Devolvidos")),
         ]
 
     def queryset(self, request, queryset):
@@ -25,5 +28,23 @@ class LoanStatusFilter(admin.SimpleListFilter):
                 return queryset.filter(
                     returned=False, due__gte=datetime.now()
                 )
-            case _:
+            case "all":
                 return queryset
+            case "not_returned" | _:
+                return queryset.filter(returned=False)
+
+    def choices(self, changelist):
+        for lookup, title in self.lookup_choices:
+            yield {
+                "selected": (
+                    self.value() == str(lookup)
+                    or (
+                        self.value() is None
+                        and str(lookup) == self.default_lookup
+                    )
+                ),
+                "query_string": changelist.get_query_string(
+                    {self.parameter_name: lookup}
+                ),
+                "display": title,
+            }
